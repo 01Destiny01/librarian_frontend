877fc429dd516956bff71c124fc40324edddb42b
new Gate <melogeronimoeduardo@gmail.com>
2025-11-23 22:07:30 +0100

    security structure and  first login view concept

---
+++ src/app/core/auth/pages/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+@Component({
+  selector: 'app-login',
+  imports: [MatInputModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+  standalone:true
+})
+export class Login {
+
+}
