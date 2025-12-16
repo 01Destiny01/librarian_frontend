bb36984c59efad7f624f3c99889c811bf4375f1f
01Destiny01 <miguelycar@gmail.com>
2025-12-16 16:29:25 +0100

    feat(librarian): login view, main  components  creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { BookCatalog } from './components/book-catalog/book-catalog';
+import { Profile } from './components/profile/profile';
+import { Login } from './core/auth/pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Login,
+  },
+  {
+    path: 'catalog',
+    component: BookCatalog,
+  },
+  {
+    path: 'notification',
+    component: BookCatalog,
+  },
+  {
+    path: 'profile',
+    component: Profile,
+  },
+
+];
