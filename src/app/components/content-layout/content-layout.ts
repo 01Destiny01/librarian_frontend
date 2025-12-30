c5e34c0b2635a78fb6bc1e1b6b7ae05e3cc80188
01Destiny01 <miguelycar@gmail.com>
2025-12-30 21:45:51 +0100

    add content-layout

---
+++ src/app/components/content-layout/content-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-layout',
+  imports: [],
+  templateUrl: './content-layout.html',
+  styleUrl: './content-layout.css',
+})
+export class ContentLayout {
+
+}
