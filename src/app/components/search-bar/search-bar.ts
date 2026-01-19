ae31ea60a165437b36ccd64f983187857f075748
01DESTINY01 <miguelycar@gmail.com>
2026-01-19 04:22:17 +0100

    create env files and books service

--- src/app/components/search-bar/search-bar.ts
+++ src/app/components/search-bar/search-bar.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './search-bar.css',
 })
 export class SearchBar {
+text: string = '';
+
 
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {};
+export const api = "https://openlibrary.org/"
+const api_search_book = "https://openlibrary.org/search.json?"
+const api_get_book_cover_by_cover_i = "https://openlibrary.org/search.json?"
